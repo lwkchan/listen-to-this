fb850bdb9597589c3fc3503494ccdf24323f574e
Laura Chan <laurachan205@gmail.com>
2018-04-04 15:49:25 +0100

    Add test for filling in form feature

---
+++ spec/features/filling_in_form_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'Filling in form' do
+  it 'allows user to fill in a form and generate a new playlist' do
+    visit('/')
+    click_on('Sign in with Spotify')
+    fill_in('', with: 'John')
+    click_on('Submit')
+    expect(page).to have_content('Here\'s what we recommend')
+  end
+end
