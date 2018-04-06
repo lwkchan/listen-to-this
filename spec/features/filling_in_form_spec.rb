81b1ccbf18291f2d55c4ee38ccfc6f434e40aba7
Laura Chan <laurachan205@gmail.com>
2018-04-06 15:40:14 +0100

    Correct filling_in_form_spec

--- spec/features/filling_in_form_spec.rb
+++ spec/features/filling_in_form_spec.rb
@@ -4,7 +4,7 @@ feature 'Filling in form' do
   it 'allows user to fill in a form and generate a new playlist' do
     visit('/')
     click_on('Sign in with Spotify')
-    fill_in('', with: 'John')
+    fill_in('', with: 'Jazz')
     click_on('Submit')
     expect(page).to have_content('Here\'s what we recommend')
   end
