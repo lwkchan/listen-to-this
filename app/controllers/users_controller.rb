32b191a5cb92ce0452c2557b336de1273a02efd6
Laura Chan <laurachan205@gmail.com>
2018-04-14 14:35:07 +0100

    Reorganise home and user#spotify views to account for updated feature test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,5 @@ class UsersController < ApplicationController
   def spotify
     #<OmniAuth::AuthHash provider="spotify" uid="123545" (in rspec)>
     @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-    p 'spotify user'
-    p @spotify_user
   end
-
 end
