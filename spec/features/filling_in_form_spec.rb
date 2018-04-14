63cb886fa41045784d56c3fda4fe6c2ffbded87e
Laura Chan <laurachan205@gmail.com>
2018-04-14 13:40:54 +0100

    Add welcome message to user#spotify view

--- spec/features/filling_in_form_spec.rb
+++ spec/features/filling_in_form_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 feature 'Filling in form' do
+
+  it 'welcomes to user to the page' do
+    visit('/')
+    click_on('Sign in with Spotify')
+    expect(page).to have_content('Welcome')
+  end
+
   it 'allows user to fill in a form and generate a new playlist' do
     visit('/')
     click_on('Sign in with Spotify')
