53e4fd14f2d18c2bedd088ac03afb364af5b54db
Laura Chan <laurachan205@gmail.com>
2018-04-14 19:50:17 +0100

    Implement top artist into reccommendation seed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
   def spotify
     @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
     @user_input = request.env['omniauth.params']['playlist']
-    seed_track1 = RSpotify::Track.search(@user_input).first
-    @tracks = RSpotify::Recommendations.generate(limit: 20, seed_tracks: [seed_track1.id]).tracks
+    seed_track = RSpotify::Track.search(@user_input).first
+    seed_artists_ids = @spotify_user.top_artists(limit: 10).map(&:id)
+    @tracks = RSpotify::Recommendations.generate(limit: 20, seed_tracks: [seed_track.id], seed_artists: [seed_artists_ids.first]).tracks
     @playlist = @spotify_user.create_playlist!("Listen to this: #{@user_input}")
     @playlist.add_tracks!(@tracks)
   end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['SPOTIFY_ID'], ENV['SPOTIFY_SECRET'], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify playlist-modify-private'
+  provider :spotify, ENV['SPOTIFY_ID'], ENV['SPOTIFY_SECRET'],
+  scope: 'user-read-email playlist-modify-public user-library-read playlist-modify-private user-top-read'
 end
