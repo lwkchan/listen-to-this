cfa3696332747e0208c2dd86b62aa593072a01c8
Laura Chan <laurachan205@gmail.com>
2018-04-14 12:30:50 +0100

    Add form to users#spotify view

--- spec/features/filling_in_form_spec.rb
+++ spec/features/filling_in_form_spec.rb
@@ -4,8 +4,8 @@ feature 'Filling in form' do
   it 'allows user to fill in a form and generate a new playlist' do
     visit('/')
     click_on('Sign in with Spotify')
-    fill_in('', with: 'Jazz')
-    click_on('Submit')
+    fill_in('playlist', with: 'Jazz')
+    click_on('Generate')
     expect(page).to have_content('Here\'s what we recommend')
   end
 end
